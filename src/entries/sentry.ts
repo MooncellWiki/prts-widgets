eead17a7689592575ebb72cc1593dce47f89a77a
daflyinbed <1677759063@qq.com>
2022-10-02 17:21:49 +0800

    feat: sentry

---
+++ src/entries/sentry.ts
@@ -0,0 +1,12 @@
+import * as Sentry from '@sentry/browser';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+    dsn: 'https://b8fa071a70cd455f9e63c105e9be7050@mt.mooncell.wiki/2',
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.0001,
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,50 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import WindiCSS from "vite-plugin-windicss";
-import { readdirSync } from "fs";
-import { join } from "path";
-import { visualizer } from "rollup-plugin-visualizer";
-const entries = readdirSync(join(__dirname, "src/entries/"));
+import { readdirSync } from 'fs';
+import { join } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import WindiCSS from 'vite-plugin-windicss';
+import { visualizer } from 'rollup-plugin-visualizer';
+const entries = readdirSync(join(__dirname, 'src/entries/'));
 const input = {};
 entries.forEach((entry) => {
-  input[entry.replace(".tsx", "")] = `src/entries/${entry}`;
+    input[entry.replace('.ts', '')] = `src/entries/${entry}`;
 });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS(),visualizer({sourcemap:true})],
-  server: {
-    hmr: {
-      host: "localhost",
-      protocol: "ws",
-    },
-  },
-  build: {
-    minify: "terser",
-    sourcemap: true,
-    manifest: true,
-    cssCodeSplit: false,
-    rollupOptions: {
-      input,
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules") &&!id.includes("hammer")) {
-            return "vendor";
-          }
-          if(id.includes("src/components/")){
-            return "vendor";
-          }
-          if(id.includes("windi")){
-            return "vendor";
-          }
+    plugins: [vue(), WindiCSS(), visualizer({ sourcemap: true })],
+    server: {
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
         },
-      },
     },
-    assetsDir: ".",
-    terserOptions:{
-      compress:{
-        passes: 10
-      }
-    }
-  },
+    build: {
+        minify: 'terser',
+        sourcemap: true,
+        manifest: true,
+        cssCodeSplit: false,
+        rollupOptions: {
+            input,
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules') && !id.includes('hammer')) {
+                        return 'vendor';
+                    }
+                    if (id.includes('src/components/')) {
+                        return 'vendor';
+                    }
+                    if (id.includes('windi')) {
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+        assetsDir: '.',
+        terserOptions: {
+            compress: {
+                passes: 10,
+            },
+        },
+    },
 });
