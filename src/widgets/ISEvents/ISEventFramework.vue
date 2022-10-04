c967a12b1d7511a1ca6206e684675fbf7b9cdca5
夕舞八弦 <1677759063@qq.com>
2022-10-04 18:05:31 +0800

    refactor(isEvent)

--- src/widgets/ISEvents/ISEventFramework.vue
+++ src/widgets/ISEvents/ISEventFramework.vue
@@ -1,33 +1,69 @@
 <template>
     <h2>{{ sceneData[0].name }}</h2>
-    <n-config-provider preflight-style-disabled :theme-overrides="{ Card: { color: '#212121' , textColor: '#fff', titleTextColor: '#fff', borderRadius: '5px', actionColor: '#343434'},
-    Breadcrumb: {itemTextColor: '#A8AFB5'},
-    Button: {textColor: '#fff'} }">
+    <n-config-provider
+        preflight-style-disabled
+        :theme-overrides="{
+            Card: {
+                color: '#212121',
+                textColor: '#fff',
+                titleTextColor: '#fff',
+                borderRadius: '5px',
+                actionColor: '#343434',
+            },
+            Breadcrumb: { itemTextColor: '#A8AFB5' },
+            Button: { textColor: '#fff' },
+        }"
+    >
         <n-space>
             <n-layout>
                 <n-layout-content>
                     <n-breadcrumb separator=">">
-                        <n-breadcrumb-item v-for="([SceneId, navText]) in sceneNav" @click="currentSceneId = SceneId">
-                            <n-icon v-if="SceneId === '0'">
+                        <n-breadcrumb-item
+                            v-for="(SceneId, index) in sceneNav"
+                            :key="index"
+                            @click="navJump(index)"
+                        >
+                            <n-icon v-if="SceneId === 0">
                                 <HomeSharp />
-                            </n-icon> {{ navText }}
+                            </n-icon>
+                            {{ sceneData[SceneId].text }}
                         </n-breadcrumb-item>
                     </n-breadcrumb>
-                    <n-card :style="{ width: '560px' }" class="relative" :title="sceneData[currentSceneId].name"
-                        size="small">
+                    <n-card
+                        :style="{ width: '560px' }"
+                        class="relative"
+                        :title="sceneData[currentSceneId].name"
+                        size="small"
+                    >
                         <template #cover>
-                            <a :href="`/images/${getImagePath(`${sceneData[currentSceneId].image}.png`)}`">
-                                <img class="lazyload img"
-                                    :data-src="`/images/${getImagePath(`${sceneData[currentSceneId].image}.png`)}`"
-                                    style="width:560px">
+                            <a
+                                :href="`/images/${getImagePath(
+                                    `${sceneData[currentSceneId].image}.png`,
+                                )}`"
+                            >
+                                <img
+                                    class="lazyload img"
+                                    :data-src="`/images/${getImagePath(
+                                        `${sceneData[currentSceneId].image}.png`,
+                                    )}`"
+                                    style="width: 560px"
+                                />
                             </a>
                         </template>
                         {{ sceneData[currentSceneId].text }}
                         <template #action>
                             <n-space vertical>
-                                <ISEventOption v-for="optionitem in sceneData[currentSceneId].options"
-                                    :optionData="optionitem" :themeColor="themeColor"
-                                    @click="currentSceneId = optionitem.dest"></ISEventOption>
+                                <ISEventOption
+                                    v-for="(item, index) in sceneData[
+                                        currentSceneId
+                                    ].options"
+                                    :key="index"
+                                    :title="item.title"
+                                    :type="item.type"
+                                    :icon="item.icon"
+                                    :description="item.description"
+                                    @click="jump(item.dest)"
+                                ></ISEventOption>
                             </n-space>
                         </template>
                     </n-card>
@@ -35,10 +71,9 @@
             </n-layout>
         </n-space>
     </n-config-provider>
-
 </template>
 <script lang="ts">
-import { defineComponent, PropType, ref } from 'vue';
+import { computed, defineComponent, PropType, ref } from 'vue';
 import {
     NConfigProvider,
     NBreadcrumb,
@@ -49,9 +84,7 @@ import {
     NCard,
     NIcon,
 } from 'naive-ui';
-import {
-    HomeSharp,
-} from '@vicons/material';
+import { HomeSharp } from '@vicons/material';
 import { getImagePath } from '../../utils/utils';
 import ISEventOption from './ISEventOption.vue';
 export default defineComponent({
@@ -68,52 +101,51 @@ export default defineComponent({
         ISEventOption,
     },
     props: {
-        sceneData: Array as PropType<
-            {
-                name?: string;
-                nav?: string;
-                index?: number;
-                image?: string;
-                text?: string;
-                options?: Array<any>;
-            }[]
-        >,
-        themeColor: String,
-    },
-    watch: {
-        currentSceneId(newEle, oldEle) {
-            this.sceneNavRefresh(newEle);
-        }
-    },
-    methods: {
-        async findIfSceneOnNav(destSceneIndex) {
-            var res = -999
-            this.sceneNav.find((nav, index, arr, dest = destSceneIndex) => {
-                if (nav[0] == dest) {
-                    res = index
-                }
-            })
-            return res
+        sceneData: {
+            type: Array as PropType<
+                {
+                    name?: string;
+                    nav?: string;
+                    index?: number;
+                    image?: string;
+                    text?: string;
+                    options?: Array<{
+                        title: string;
+                        type: string;
+                        icon: string;
+                        description: string;
+                        dest: number;
+                    }>;
+                }[]
+            >,
+            default: [],
         },
-        async sceneNavRefresh(newSceneId) {
-            var newScene = this.sceneData[newSceneId || 0];
-            console.log("newSceneId", newSceneId)
-            var findRepetedSceneOnNav = this.findIfSceneOnNav(newSceneId);
-            console.log("findRepetedSceneOnNav", findRepetedSceneOnNav)
-            if (findRepetedSceneOnNav) {
-
-            } else {
-                this.sceneNav.push([newSceneId, newScene.nav])
-            }
-        }
     },
-    setup(props) {
-        const sceneNav = ref([['0', "开始"]]);
-        const currentSceneId = ref('0');
+    setup() {
+        const sceneNav = ref<Array<number>>([0]);
+        const currentSceneId = ref(0);
+        function jump(id: number) {
+            let index = sceneNav.value.findIndex((v) => v == id);
+            if (index == -1) {
+                sceneNav.value.push(id);
+            } else if (index + 1 < sceneNav.value.length) {
+                sceneNav.value.splice(index + 1);
+            }
+            currentSceneId.value = id;
+        }
+        function navJump(index: number) {
+            if (index == sceneNav.value.length - 1) {
+                return;
+            }
+            currentSceneId.value = sceneNav.value[index];
+            sceneNav.value.splice(index + 1);
+        }
         return {
             getImagePath,
             sceneNav,
             currentSceneId,
+            jump,
+            navJump,
         };
     },
 });
--- src/widgets/ISEvents/ISEventOption.vue
+++ src/widgets/ISEvents/ISEventOption.vue
@@ -1,61 +1,48 @@
 <template>
-    <n-card :style="{ cursor: 'pointer' }" :title="optionData.title" size="small" :header-style="{ height:'3em' }">
-        <template #header-extra v-if="optionData.icon">
-            <n-avatar v-if="optionData.type === 'simple'" color="#212121" object-fit='scale-down' :size="40"
-                :src="`/images/${getImagePath(`集成战略_选项_${optionData.icon}.png`)}`" />
-            <n-avatar v-if="optionData.type === 'item'" color="#212121" object-fit='scale-down' :size="40"
-                :src="`/images/${getImagePath(`集成战略_道具_${optionData.icon}.png`)}`" />
+    <n-card
+        :style="{ cursor: 'pointer' }"
+        :title="title"
+        size="small"
+        :header-style="{ height: '3em' }"
+    >
+        <template v-if="icon" #header-extra>
+            <n-avatar
+                v-if="type === 'simple'"
+                color="#212121"
+                object-fit="scale-down"
+                :size="40"
+                :src="`/images/${getImagePath(`集成战略_选项_${icon}.png`)}`"
+            />
+            <n-avatar
+                v-if="type === 'item'"
+                color="#212121"
+                object-fit="scale-down"
+                :size="40"
+                :src="`/images/${getImagePath(`集成战略_道具_${icon}.png`)}`"
+            />
         </template>
-        {{optionData.description}}
+        {{ description }}
     </n-card>
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
-import {
-    NCard,
-    NAvatar,
-    NIcon,
-} from 'naive-ui';
- import {
-     ChevronRightSharp,
-     InfoSharp,
-     HelpSharp,
-     WarningSharp,
-     DangerousSharp,
-     DoDisturbSharp,
-     DoNotDisturbSharp,
- } from '@vicons/material';
+import { defineComponent } from 'vue';
+import { NCard, NAvatar } from 'naive-ui';
 import { getImagePath } from '../../utils/utils';
 export default defineComponent({
     components: {
         NCard,
         NAvatar,
-        NIcon,
-        
-        ChevronRightSharp,
-        InfoSharp,
-        HelpSharp,
-        WarningSharp,
-        DangerousSharp,
-        DoDisturbSharp,
-        DoNotDisturbSharp,
     },
     props: {
-        optionData: Array as PropType<
-            {
-                title?: string;
-                type?: string;
-                icon?: string;
-                description?: string;
-                dest?: Number;
-            }[]
-        >,
+        title: String,
+        type: String,
+        icon: String,
+        description: String,
     },
-    setup(props) {
-        //console.log("optiondata", props.optionData)
+    setup() {
         return {
             getImagePath,
-        }
-    }
-})
+        };
+    },
+});
 </script>
