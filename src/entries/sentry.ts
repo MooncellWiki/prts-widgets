acae388f3d410e6b922650af160c3bfddf9fe86d
StarHeartHunt <starheart233@gmail.com>
2022-11-10 21:22:55 +0800

    chore: prevent console apis get hooked

--- src/entries/sentry.ts
+++ src/entries/sentry.ts
@@ -3,7 +3,10 @@ import { BrowserTracing } from '@sentry/tracing'
 
 Sentry.init({
   dsn: 'https://b8fa071a70cd455f9e63c105e9be7050@mt.mooncell.wiki/2',
-  integrations: [new BrowserTracing()],
+  integrations: [
+    new BrowserTracing(),
+    new Sentry.Integrations.Breadcrumbs({ console: false }),
+  ],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
