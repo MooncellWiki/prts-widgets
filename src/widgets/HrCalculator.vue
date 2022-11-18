6b62dd551c1531e3b96439a1fd1bdf0f25232e97
StarHeartHunt <starheart233@gmail.com>
2022-11-18 09:51:05 +0800

    feat: init `HrCalculator`

---
+++ src/widgets/HrCalculator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {},
+  setup(props) {
+    console.log(props)
+  },
+})
+</script>
