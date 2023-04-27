e8daf20d6c46054c99b8d83f89f954c28e7e600e
daflyinbed <1677759063@qq.com>
2023-04-27 21:40:33 +0800

    feat: auto detect host, add sampleRate

--- src/entries/sentry.ts
+++ src/entries/sentry.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/browser'
 import { BrowserTracing } from '@sentry/tracing'
 
 Sentry.init({
-  dsn: 'https://b8fa071a70cd455f9e63c105e9be7050@mt.mooncell.wiki/2',
+  dsn: location.host.includes('prts') ? 'https://b8fa071a70cd455f9e63c105e9be7050@mt.mooncell.wiki/2' : 'https://dae44b0169d14d9cab6a30a81c292fd4@mt.mooncell.wiki/8',
   integrations: [
     new BrowserTracing(),
     new Sentry.Integrations.Breadcrumbs({ console: false }),
@@ -12,6 +12,7 @@ Sentry.init({
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 0.0001,
+  sampleRate: 0.01,
 })
 
 window.Sentry = {
