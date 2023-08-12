3105cd661c45135bd7e548acab2922038a009490
StarHeartHunt <starheart233@gmail.com>
2023-08-12 14:52:08 +0800

    build: remove unused pinia ref

--- src/entries/EnemiesListV2.ts
+++ src/entries/EnemiesListV2.ts
@@ -1,12 +1,7 @@
-import { createPinia } from 'pinia'
 import 'virtual:uno.css'
 import { createApp } from 'vue'
 import EnemiesListV2 from '../widgets/EnemiesListV2/index.vue'
 
 const ele = document.getElementById('root')
-if (ele) {
-  const app = createApp(EnemiesListV2, {})
-  const pinia = createPinia()
-  app.use(pinia)
-  app.mount(ele)
-}
+if (ele)
+  createApp(EnemiesListV2, {}).mount(ele)
