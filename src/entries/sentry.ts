ce192f4e0946f1ab6714244ff2da77ffdee190a2
StarHeart <starheart233@gmail.com>
2023-08-14 12:07:17 +0800

    chore(sentry): update dsn

--- src/entries/sentry.ts
+++ src/entries/sentry.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/browser'
 import { BrowserTracing } from '@sentry/tracing'
 
 Sentry.init({
-  dsn: location.host.includes('prts') ? 'https://b8fa071a70cd455f9e63c105e9be7050@mt.mooncell.wiki/2' : 'https://dae44b0169d14d9cab6a30a81c292fd4@mt.mooncell.wiki/8',
+  dsn: location.host.includes('prts') ? 'https://b8fa071a70cd455f9e63c105e9be7050@ingest.sentry.mooncell.wiki/2' : 'https://dae44b0169d14d9cab6a30a81c292fd4@ingest.sentry.mooncell.wiki/8',
   integrations: [
     new BrowserTracing(),
     new Sentry.Integrations.Breadcrumbs({ console: false }),
