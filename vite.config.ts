176c0a479e229f4f5592156f865fda590b532d59
StarHeartHunt <starheart233@gmail.com>
2023-08-17 19:56:57 +0800

    build: update vite chunk config

--- vite.config.ts
+++ vite.config.ts
@@ -51,6 +51,12 @@ export default defineConfig({
 
           if (id.includes('uno'))
             return 'vendor'
+
+          if (id.includes('src/utils/'))
+            return 'vendor'
+
+          if (id.includes('src/stores/'))
+            return 'vendor'
         },
         chunkFileNames: '[name].[hash].js',
         entryFileNames: chunk => nohashEntries.includes(chunk.name) ? '[name].js' : '[name].[hash].js',
