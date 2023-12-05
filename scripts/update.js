832d6740ffe30a0f4215dcda24737d532c6925d4
StarHeartHunt <starheart233@gmail.com>
2023-12-05 16:27:58 +0800

    build: use html mode to build for prod

--- scripts/update.js
+++ scripts/update.js
@@ -6,50 +6,29 @@ import { fileURLToPath } from "node:url";
 import { edit, login } from "./api.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-function readDist() {
-  const json = fs.readFileSync(join(__dirname, "../dist/.vite/manifest.json"), {
-    encoding: "utf8",
-  });
-  const manifest = JSON.parse(json);
-  const result = {};
-  for (const v of Object.values(manifest)) {
-    const name = v.file.split(".")[0];
-    result[name] = {
-      fileName: v.file,
-      content: fs.readFileSync(join(__dirname, "../dist/", v.file), {
-        encoding: "utf8",
-      }),
-    };
-  }
-  return result;
-}
+
 async function main() {
   if (process.argv[2] === "-h" || process.argv.length < 3) {
     console.log("node .\\cli\\update.mjs username password");
     return;
   }
-  const dist = readDist();
-  const cssDom = `<link rel="stylesheet" href="https://static.prts.wiki/widgets/release/${dist.style.fileName}">`;
   const username = process.argv[2];
   const password = process.argv[3];
   await login(username, password);
-  const tmpls = fs.readdirSync(join(__dirname, "../templates/"));
+  const tmpls = fs.readdirSync(join(__dirname, "../dist/templates/"));
   await Promise.allSettled(
     tmpls.map((tmpl) => {
       const name = tmpl.replace(".html", "");
-      let content = fs.readFileSync(join(__dirname, "../templates/", tmpl), {
-        encoding: "utf8",
-      });
-
-      const script = dist[name].content.trim();
-      content = content
-        .replaceAll("__CSS_CONTENT__", cssDom)
-        .replaceAll("__SCRIPT_CONTENT__", script)
-        .replaceAll(
-          "__SCRIPT_PATH__",
-          `https://static.prts.wiki/widgets/release/${dist[name].fileName}`,
-        );
-      return edit(`Widget:${name}`, content);
+      let content = fs.readFileSync(
+        join(__dirname, "../dist/templates/", tmpl),
+        {
+          encoding: "utf8",
+        },
+      );
+      return edit(
+        `Widget:${name}`,
+        content.replace("<head>", "").replace("</head>", ""),
+      );
     }),
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -8,98 +8,112 @@ import { visualizer } from "rollup-plugin-visualizer";
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 
-const TARGET = ["es2020", "edge88", "firefox78", "chrome87", "safari14"];
+const TARGET = ["chrome70", "edge81", "firefox70", "safari12", "ios12"];
 
 const entries = readdirSync(
   join(dirname(fileURLToPath(import.meta.url)), "src/entries/"),
 );
+const templates = readdirSync(
+  join(dirname(fileURLToPath(import.meta.url)), "templates/"),
+);
 const nohashEntries = new Set(["DisplayController"]);
+
 const input: Record<string, string> = {};
 for (const entry of entries) {
   input[entry.replace(".ts", "")] = `src/entries/${entry}`;
 }
 
+const templatesInput: Record<string, string> = {};
+for (const template of templates) {
+  templatesInput[template.replace(".html", "")] = `templates/${template}`;
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(dirname(fileURLToPath(import.meta.url)), "./src"),
-    },
-  },
-  plugins: [
-    vue(),
-    UnoCSS(),
-    legacy({
-      targets: TARGET,
-      modernPolyfills: true,
-      renderLegacyChunks: false,
-    }),
-    visualizer({ sourcemap: true }),
-  ],
-  server: {
-    port: 8080,
-    hmr: {
-      host: "localhost",
-      protocol: "ws",
-    },
-  },
-  build: {
-    minify: "terser",
-    sourcemap: true,
-    manifest: true,
-    cssCodeSplit: false,
-    rollupOptions: {
-      input,
-      output: {
-        sourcemapBaseUrl: "https://static.prts.wiki/widgets/release/",
-        manualChunks(id) {
-          if (id.includes("sentry")) return "sentry";
-          if (id.includes("naive-ui")) return "naive-ui";
-          if (id.includes("hammer")) return;
-          if (id.includes("node_modules")) return "vendor";
-          if (id.includes("uno")) return "vendor";
-          if (id.includes("src/components/")) return "common";
-          if (id.includes("src/utils/")) return "common";
-          if (id.includes("src/stores/")) return "common";
-        },
-        chunkFileNames: "[name].[hash].js",
-        entryFileNames: (chunk) =>
-          nohashEntries.has(chunk.name) ? "[name].js" : "[name].[hash].js",
-        assetFileNames: "[name].[hash].[ext]",
+export default defineConfig(({ command }) => {
+  const useHtmlMode = command === "build";
+
+  return {
+    base: useHtmlMode ? "https://static.prts.wiki/widgets/release" : "/",
+    resolve: {
+      alias: {
+        "@": resolve(dirname(fileURLToPath(import.meta.url)), "./src"),
       },
-      plugins: [
-        {
-          name: "prts",
-          generateBundle(options, bundle) {
-            const bundles = Object.keys(bundle);
-            const fileNames = ["vendor", "naive-ui", "common"].map((name) => {
-              return bundles.find((v) => v.startsWith(name))!;
-            });
-
-            for (const key of Object.keys(bundle)) {
-              const chunk = bundle[key];
-              if (chunk.type !== "chunk") continue;
-
-              if (chunk.fileName.startsWith("SpineViewer")) {
-                // SpineViewer 不需要改导入路径
-                continue;
-              }
-              for (const name of fileNames) {
-                chunk.code = chunk.code.replaceAll(
-                  `./${name}`,
-                  `https://static.prts.wiki/widgets/release/${name}`,
-                );
-              }
-            }
+    },
+    plugins: [
+      vue(),
+      UnoCSS(),
+      legacy({
+        targets: TARGET,
+        modernPolyfills: true,
+        renderLegacyChunks: false,
+      }),
+      visualizer({ sourcemap: true }),
+    ],
+    server: {
+      port: 8080,
+      hmr: {
+        host: "localhost",
+        protocol: "ws",
+      },
+    },
+    build: {
+      minify: "terser",
+      sourcemap: true,
+      manifest: true,
+      cssCodeSplit: false,
+      rollupOptions: {
+        input: useHtmlMode ? templatesInput : input,
+        output: {
+          sourcemapBaseUrl: "https://static.prts.wiki/widgets/release/",
+          manualChunks(id) {
+            if (id.includes("sentry")) return "sentry";
+            if (id.includes("naive-ui")) return "naive-ui";
+            if (id.includes("hammer")) return;
+            if (id.includes("node_modules")) return "vendor";
+            if (id.includes("uno")) return "vendor";
+            if (id.includes("src/components/")) return "common";
+            if (id.includes("src/utils/")) return "common";
+            if (id.includes("src/stores/")) return "common";
           },
+          chunkFileNames: "[name].[hash].js",
+          entryFileNames: (chunk) =>
+            nohashEntries.has(chunk.name) ? "[name].js" : "[name].[hash].js",
+          assetFileNames: "[name].[hash].[ext]",
         },
-      ],
-    },
-    assetsDir: ".",
-    terserOptions: {
-      compress: {
-        passes: 10,
+        plugins: [
+          {
+            name: "prts",
+            generateBundle(options, bundle) {
+              const bundles = Object.keys(bundle);
+              const fileNames = ["vendor", "naive-ui", "common"].map((name) => {
+                return bundles.find((v) => v.startsWith(name))!;
+              });
+
+              for (const key of Object.keys(bundle)) {
+                const chunk = bundle[key];
+                if (chunk.type !== "chunk") continue;
+
+                if (chunk.fileName.startsWith("SpineViewer")) {
+                  // SpineViewer 不需要改导入路径
+                  continue;
+                }
+                for (const name of fileNames) {
+                  chunk.code = chunk.code.replaceAll(
+                    `./${name}`,
+                    `https://static.prts.wiki/widgets/release/${name}`,
+                  );
+                }
+              }
+            },
+          },
+        ],
+      },
+      assetsDir: ".",
+      terserOptions: {
+        compress: {
+          passes: 10,
+        },
       },
     },
-  },
+  };
 });
