7db46ff837d44888e9cb6fd3ff4f6b908db6b754
Yanstime <862415702@qq.com>
2023-12-22 01:59:18 +0800

    feat(MedalShowcase): It Can't Work Edition

---
+++ src/entries/MedalShowcase.ts
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+
+import "virtual:uno.css";
+import MedalShowcase from "../widgets/MedalList/MedalShowcase.vue";
+
+const eles = document.querySelectorAll(
+  ".medal-showcase",
+) as unknown as HTMLCollectionOf<HTMLElement>;
+
+for (const showcaseEle of Array.from(eles)) {
+  const data = showcaseEle.dataset;
+  createApp(MedalShowcase, {
+    medal: data.medal || "",
+    medalGroup: data.medalgroup || "",
+  }).mount(showcaseEle);
+}
---
+++ src/widgets/MedalList/MedalShowcase.vue
@@ -0,0 +1,228 @@
+<script lang="ts">
+import { computed, defineComponent, onMounted, ref } from "vue";
+
+import {
+  NButton,
+  NCard,
+  NConfigProvider,
+  NGrid,
+  NGridItem,
+  NLayout,
+  NEmpty,
+  NTooltip,
+} from "naive-ui";
+
+import { getNaiveUILocale } from "@/utils/i18n";
+import { useTheme } from "@/utils/theme";
+
+import { getImagePath } from "../../utils/utils";
+
+import MedalComponent from "./Medal.vue";
+import MedalGroupComponent from "./MedalGroup.vue";
+import type { MedalMetaData } from "./types";
+import { getMedalMetaData } from "./utils";
+function openMedalPage() {
+  window.open("https://prts.wiki/w/光荣之路", "_blank");
+}
+export default defineComponent({
+  components: {
+    MedalComponent,
+    MedalGroupComponent,
+    NButton,
+    NCard,
+    NConfigProvider,
+    NLayout,
+    NGrid,
+    NGridItem,
+    NEmpty,
+    NTooltip,
+  },
+  props: {
+    medal: {
+      type: String,
+      required: true,
+    },
+    medalGroup: {
+      type: String,
+      required: true,
+    },
+    spoiler: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const medalMetaData = ref<MedalMetaData>({
+      medal: {},
+      medalGroup: {},
+      category: {},
+    });
+
+    onMounted(async () => {
+      medalMetaData.value = await getMedalMetaData();
+    });
+
+    const medalList = computed<string[]>(() => {
+      const res = props.medal.split(";");
+      return res.length === 1 && res[0] === "" ? [] : res;
+    });
+    const medalGroupList = computed(() => {
+      const res = props.medalGroup.split(";");
+      return res.length === 1 && res[0] === "" ? [] : res;
+    });
+
+    const filteredMedalData = computed(() => {
+      const medalGroup = Object.fromEntries(
+        Object.entries(medalMetaData.value.medalGroup).filter(([gid]) => {
+          if (medalGroupList.value.includes(gid)) {
+            return true;
+          }
+          return false;
+        }),
+      );
+
+      const medal = Object.fromEntries(
+        Object.entries(medalMetaData.value.medal).filter(([id]) => {
+          if (medalList.value.includes(id)) {
+            return true;
+          }
+          return false;
+        }),
+      );
+
+      return {
+        medal,
+        medalGroup,
+      };
+    });
+
+    const generateGroupMedalData = (medalGroupId: string) => {
+      return Object.values(
+        medalMetaData.value.medalGroup[medalGroupId].medal,
+      ).map((id) => {
+        return medalMetaData.value.medal[id];
+      });
+    };
+
+    const spoilerManualUnlocked = ref(!props.spoiler);
+
+    const { theme, toggleDark } = useTheme();
+
+    return {
+      i18nConfig: getNaiveUILocale(),
+      medalList,
+      medalGroupList,
+      filteredMedalData,
+      spoilerManualUnlocked,
+      theme,
+      toggleDark,
+      getImagePath,
+      openMedalPage,
+      generateGroupMedalData,
+    };
+  },
+});
+</script>
+
+<template>
+  <NConfigProvider
+    preflight-style-disabled
+    :theme="theme"
+    :locale="i18nConfig.locale"
+    :date-locale="i18nConfig.dateLocale"
+  >
+    <NLayout class="antialiased max-w-200">
+      <NCard>
+        <template #header>
+          <img
+            :src="`/images/${getImagePath('图标_光荣之路.png')}`"
+            width="25"
+          />&nbsp;&nbsp;
+          <n-tooltip trigger="hover">
+            <template #trigger>
+              <NButton
+                text
+                text-color="#2f2f2f"
+                class="text-1.15rem visited:color-none hover:decoration-none focus:decoration-none"
+                tag="a"
+                @click="openMedalPage()"
+              >
+                光荣之路 <span class="mdi mdi-chevron-right"
+              /></NButton>
+            </template>
+            点击前往【光荣之路】主页面
+          </n-tooltip>
+        </template>
+        <NGrid
+          v-if="medalList.length === 0 && medalGroupList.length === 0"
+          cols="1"
+          :x-gap="12"
+          :y-gap="8"
+          responsive="screen"
+        >
+          <NGridItem span="1">
+            <NEmpty>
+              <template #default>
+                <div class="text-center">
+                  并没有任何蚀刻章，你是不是忘了配置了？<br />使用 medalgroup 与
+                  medal 来配置要显示的蚀刻章套组与单章<br />使用半角分号（;）分隔
+                  ID
+                </div>
+              </template>
+            </NEmpty>
+          </NGridItem>
+        </NGrid>
+        <NGrid
+          v-else-if="!spoilerManualUnlocked"
+          cols="1"
+          :x-gap="12"
+          :y-gap="8"
+          responsive="screen"
+        >
+          <NGridItem span="1 l:2">
+            <NEmpty class="bg-#424242">
+              <template #icon>
+                <div class="text-center">
+                  <span class="mdi mdi-lock color-white p-0 font-size-2rem" />
+                </div>
+              </template>
+              <template #default>
+                <div class="text-center color-white pt-2">
+                  以下内容需要验证权限后查阅<br /><span
+                    class="mdi mdi-information-variant-circle"
+                  />&nbsp;本活动关联蚀刻章已加密，请自行决定是否查看。
+                </div>
+              </template>
+              <template #extra>
+                <div class="text-center color-white pb-2">
+                  <NButton
+                    color="white"
+                    text-color="#424242"
+                    @click="
+                      () => {
+                        spoilerManualUnlocked = true;
+                      }
+                    "
+                  >
+                    <span class="mdi mdi-key-variant" />&nbsp;移除加密
+                  </NButton>
+                </div>
+              </template>
+            </NEmpty>
+          </NGridItem>
+        </NGrid>
+        <NGrid v-else cols="1" :x-gap="12" :y-gap="8" responsive="screen">
+          <NGridItem v-for="medalId in medalList" :key="medalId">
+            <MedalComponent :medal-data="filteredMedalData.medal[medalId]" />
+          </NGridItem>
+          <NGridItem v-for="medalGroupId in medalGroupList" :key="medalGroupId">
+            <MedalGroupComponent
+              :group-data="filteredMedalData.medalGroup[medalGroupId]"
+              :medal-data-list="generateGroupMedalData(medalGroupId)"
+            />
+          </NGridItem>
+        </NGrid>
+      </NCard>
+    </NLayout>
+  </NConfigProvider>
+</template>
