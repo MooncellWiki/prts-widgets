1148f5443e0d58bc3b088883260deb7061dafde6
StarHeartHunt <starheart233@gmail.com>
2024-03-18 20:50:46 +0800

    style: use constant from utils

--- src/entries/PenguinWidget.ts
+++ src/entries/PenguinWidget.ts
@@ -1,6 +1,8 @@
 import "virtual:uno.css";
 import { createApp } from "vue";
 
+import { STATIC_ENDPOINT } from "@/utils/consts";
+
 import PenguinWidget from "../widgets/PenguinWidget.vue";
 
 const ele = document.querySelector<HTMLElement>("#penguin-widget");
@@ -10,7 +12,7 @@ const isAct = !!Number.parseInt(ele?.dataset.isAct || "0");
 const language = navigator.language.split("-")[0];
 
 if (ele) {
-  fetch("https://static.prts.wiki/20230731ua/display_config_v2.json")
+  fetch(new URL("/skland/display_config_v3.json", STATIC_ENDPOINT))
     .then((response) => {
       if (!response.ok)
         throw new Error("[PenguinWidget] Received non-200 response");
