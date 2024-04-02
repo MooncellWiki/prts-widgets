7ef66693ed07e19085d6e562b44a4dc1ff7b98ab
StarHeartHunt <starheart233@gmail.com>
2024-04-02 15:57:45 +0800

    ci: update prune script

--- scripts/prune.js
+++ scripts/prune.js
@@ -4,13 +4,13 @@ import { fileURLToPath } from "node:url";
 
 import OSS from "ali-oss";
 
+const isDistFiles = (file) => file.endsWith(".js") || file.endsWith(".js.map");
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const localJSFiles = new Set(
-  fs
-    .readdirSync(join(__dirname, "../dist/"))
-    .filter((file) => file.endsWith(".js")),
+const localDistFiles = new Set(
+  fs.readdirSync(join(__dirname, "../dist/")).filter((ele) => isDistFiles(ele)),
 );
-console.log("[INFO] local js files:", localJSFiles);
+console.log("[INFO] local js files:", localDistFiles);
 
 const { REGION, ACCESS_KEY_ID, ACCESS_KEY_SECRET, BUCKET, REMOTE_PATH } =
   process.env;
@@ -23,13 +23,19 @@ const store = new OSS({
 
 //List REMOTE_PATH dir objects, not including subdirs
 const result = await store.listV2({ prefix: REMOTE_PATH, delimiter: "/" });
-const removingFiles = result.objects.filter(
-  (object) =>
-    object.name.endsWith(".js") &&
-    !localJSFiles.has(object.name.replace(REMOTE_PATH, "")),
+console.log(
+  "[INFO] list result:",
+  result.objects.map((object) => object.name),
 );
+
+const removingFiles = result.objects
+  .filter(
+    (object) =>
+      isDistFiles(object.name) &&
+      !localDistFiles.has(object.name.replace(REMOTE_PATH, "")),
+  )
+  .map((file) => file.name);
 console.log("[INFO] removing files:", removingFiles);
-const deleteResult = await store.deleteMulti(
-  removingFiles.map((file) => file.name),
-);
-console.log("[INFO] delete result:", deleteResult);
+
+const deleteResult = await store.deleteMulti(removingFiles);
+console.log("[INFO] delete result:", deleteResult.deleted);
