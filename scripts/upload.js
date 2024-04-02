435e8e79f4c23e40d9e70059bdaac86a65d2f8e4
StarHeartHunt <starheart233@gmail.com>
2024-04-02 16:18:50 +0800

    ci: use self built script to upload files

---
+++ scripts/upload.js
@@ -0,0 +1,27 @@
+import fs from "node:fs";
+import { dirname, join, posix } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import OSS from "ali-oss";
+
+const isDistFiles = (file) => file.endsWith(".js") || file.endsWith(".js.map");
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const distFiles = new Set(
+  fs.readdirSync(join(__dirname, "../dist/")).filter((ele) => isDistFiles(ele)),
+);
+console.log("[INFO] local js files:", distFiles);
+
+const { REGION, ACCESS_KEY_ID, ACCESS_KEY_SECRET, BUCKET, REMOTE_PATH } =
+  process.env;
+const store = new OSS({
+  region: REGION,
+  accessKeyId: ACCESS_KEY_ID,
+  accessKeySecret: ACCESS_KEY_SECRET,
+  bucket: BUCKET,
+});
+
+for (const file of distFiles) {
+  const result = await store.put(posix.join(REMOTE_PATH, file), file);
+  console.log("[INFO] Uploaded", result.res.status, result.name);
+}
