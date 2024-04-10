b01146ddfa92fb3bb75dbe3352daa07b5b4d733f
daflyinbed <1677759063@qq.com>
2024-04-11 00:54:28 +0800

    feat: ignore more

--- src/entries/sentry.ts
+++ src/entries/sentry.ts
@@ -18,8 +18,49 @@ Sentry.init({
 
   sampleRate: 0.01,
 
-  ignoreErrors: ["google", "baidu"],
-
+  ignoreErrors: [
+    // ads and statistics
+    "google",
+    "baidu",
+    // https://docs.sentry.io/platforms/javascript/configuration/filtering/#decluttering-sentry
+    // copy from https://gist.github.com/impressiver/5092952
+    // Random plugins/extensions
+    "top.GLOBALS",
+    // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error.html
+    "originalCreateNotification",
+    "canvas.contentDocument",
+    "MyApp_RemoveAllHighlights",
+    "http://tt.epicplay.com",
+    "Can't find variable: ZiteReader",
+    "jigsaw is not defined",
+    "ComboSearch is not defined",
+    "http://loading.retry.widdit.com/",
+    "atomicFindClose",
+    // Facebook borked
+    "fb_xd_fragment",
+    // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to reduce this. (thanks @acdha)
+    // See http://stackoverflow.com/questions/4113268/how-to-stop-javascript-injection-from-vodafone-proxy
+    "bmi_SafeAddOnload",
+    "EBCallBackMessageReceived",
+    // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+    "conduitPage",
+  ],
+  denyUrls: [
+    // Facebook flakiness
+    /graph\.facebook\.com/i,
+    // Facebook blocked
+    /connect\.facebook\.net\/en_us\/all\.js/i,
+    // Woopra flakiness
+    /eatdifferent\.com\.woopra-ns\.com/i,
+    /static\.woopra\.com\/js\/woopra\.js/i,
+    // Chrome extensions
+    /extensions\//i,
+    /^chrome:\/\//i,
+    // Other plugins
+    /127\.0\.0\.1:4001\/isrunning/i, // Cacaoweb
+    /webappstoolbarba\.texthelp\.com\//i,
+    /metrics\.itunes\.apple\.com\.edgesuite\.net\//i,
+  ],
   // pref
   tracesSampleRate: 0.0001,
   // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
