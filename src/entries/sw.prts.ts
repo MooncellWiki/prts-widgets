df1590e3dccebca9412a1c1a39a94a46f8a84749
StarHeartHunt <starheart233@gmail.com>
2024-08-14 21:12:52 +0800

    chore: remove unexpected service worker rule

--- src/entries/sw.prts.ts
+++ src/entries/sw.prts.ts
@@ -1,10 +1,5 @@
-import { registerRoute, setDefaultHandler } from "workbox-routing";
-import {
-  StaleWhileRevalidate,
-  CacheFirst,
-  NetworkFirst,
-  NetworkOnly,
-} from "workbox-strategies";
+import { registerRoute } from "workbox-routing";
+import { NetworkFirst } from "workbox-strategies";
 registerRoute(/\.php/, new NetworkFirst({ networkTimeoutSeconds: 5 }));
 registerRoute(
   /^https:\/\/(m\.)?(fgo|prts).wiki\/w\//,
@@ -14,9 +9,3 @@ registerRoute(
   /^https:\/\/torappu.prts.wiki\/api/,
   new NetworkFirst({ networkTimeoutSeconds: 5 }),
 );
-registerRoute(/^https:\/\/static\.prts\.wiki/, new CacheFirst());
-
-registerRoute(/^https:\/\/hm\.baidu\.com.*/, new NetworkOnly());
-registerRoute(/^https:\/\/www\.google\.com.*/, new NetworkOnly());
-registerRoute(/^https:\/\/ingest\.sentry\.mooncell\.wiki.*/, new NetworkOnly());
-setDefaultHandler(new StaleWhileRevalidate());
