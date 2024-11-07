8b325207952f3ace3007b26dc4126297370c57ae
StarHeartHunt <starheart233@gmail.com>
2024-11-07 20:09:32 +0800

    chore(unocss): use unocss eslint flat config instead

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from "@eslint/js";
-import pluginUnoCSS from "@unocss/eslint-plugin";
+import unocss from "@unocss/eslint-config/flat";
 import pluginImport from "eslint-plugin-import-x";
 import configPrettierRecommended from "eslint-plugin-prettier/recommended";
 import pluginUnicorn from "eslint-plugin-unicorn";
@@ -311,17 +311,6 @@ export const unicorn = [
 
 export const ignores = {
   ignores: ["**/spine/runtime", "**/.husky", "**/dist"],
-};
-
-export const unocss = {
-  name: "unocss",
-  plugins: {
-    unocss: pluginUnoCSS,
-  },
-  rules: {
-    "unocss/order": "warn",
-    "unocss/blocklist": "error",
-  },
 };
 
 export default [
