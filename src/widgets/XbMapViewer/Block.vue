b807fddab4c85b2a38f483b0c1d90f4b7ec7faa7
Yanstime <862415702@qq.com>
2025-03-30 12:26:43 +0800

    feat(XbMapViewer): load style and config from wiki page (#207)
    * feat(XbMapViewer): data split
    
    * fix(XbMapViewer): await bug
    
    * refactor
    
    * Update src/widgets/XbMapViewer/Map.vue
    
    ---------
    
    Co-authored-by: 夕舞八弦 <1677759063@qq.com>
    Co-authored-by: StarHeart <starheart233@gmail.com>

--- src/widgets/XbMapViewer/Block.vue
+++ src/widgets/XbMapViewer/Block.vue
@@ -3,7 +3,7 @@ import { computed, onMounted, ref } from "vue";
 
 import { getImagePath } from "@/utils/utils";
 
-import { TipMap, bgMap, classMap } from "./consts";
+import { type BlockDefine } from "./consts";
 
 const props = withDefaults(
   defineProps<{
@@ -11,6 +11,7 @@ const props = withDefaults(
     tileHeightType: string;
     tokens?: string[];
     black?: string;
+    blockmap: Record<string, BlockDefine>;
   }>(),
   {
     black: "",
@@ -18,14 +19,17 @@ const props = withDefaults(
 );
 
 const self = ref();
+
 const style = computed(() => {
-  const result = {} as Record<string, string>;
-  if (bgMap[props.tile!])
-    result.backgroundImage = `url(${getImagePath(bgMap[props.tile!])})`;
+  const result: Record<string, string> = {};
+
+  if (props.blockmap[props.tile!]?.img) {
+    result.backgroundImage = `url(${getImagePath(props.blockmap[props.tile!].img!)})`;
+  }
 
   for (const token of props.tokens || []) {
-    if (token && bgMap[token])
-      result.backgroundImage = `url(${getImagePath(bgMap[token])})`;
+    if (props.blockmap[token]?.img)
+      result.backgroundImage = `url(${getImagePath(props.blockmap[token].img)})`;
   }
 
   if (
@@ -42,11 +46,14 @@ const style = computed(() => {
 
 onMounted(() => {
   let content = "";
-  if (props.tile && TipMap[props.tile]) content += TipMap[props.tile];
+  if (props.tile && props.blockmap[props.tile]?.desc)
+    content += props.blockmap[props.tile].desc;
 
   for (const token of props.tokens || []) {
-    if (token && TipMap[token])
-      content += content ? `<br/> ${TipMap[token]}` : ` ${TipMap[token]}`;
+    if (token && props.blockmap[token]?.desc)
+      content += content
+        ? `<br/> ${props.blockmap[token].desc}`
+        : ` ${props.blockmap[token].desc}`;
   }
 
   if (content.length > 0) {
@@ -77,14 +84,14 @@ onMounted(() => {
   >
     <template v-if="tokens">
       <template v-for="(token, i) in tokens" :key="i">
-        <span v-if="classMap[token!]">
-          <i :class="classMap[token!]" />
+        <span v-if="blockmap[token!]?.icon">
+          <i :class="blockmap[token!].icon" />
         </span>
       </template>
     </template>
     <template v-else>
-      <span v-if="classMap[tile!]">
-        <i :class="classMap[tile!]" />
+      <span v-if="blockmap[tile!]?.icon">
+        <i :class="blockmap[tile!].icon" />
       </span>
     </template>
   </div>
@@ -134,182 +141,5 @@ onMounted(() => {
 
 .empty {
   background-color: transparent;
-}
-
-.start,
-.flystart {
-  background-color: indianred;
-  border: 2px solid darkred;
-}
-
-.start span,
-.flystart span,
-.ballis span,
-.ore span,
-.airsup span,
-.xbalis span,
-.xbbillb span {
-  color: darkred;
-}
-
-.end {
-  background-color: skyblue;
-  border: 2px solid dodgerblue;
-}
-
-.end span {
-  color: dodgerblue;
-}
-
-.floor {
-  background-color: darkgray;
-  border: 1px dashed yellow;
-  box-shadow: inset 0 0 0 2px black;
-}
-
-.floor span {
-  color: white;
-}
-
-.road {
-  background-color: darkgray;
-  border: 2px dotted dimgray;
-}
-
-.wall {
-  background-color: lightgray;
-  border: 2px dotted dimgray;
-  box-shadow: 5px 5px 8px black;
-  z-index: 2;
-}
-
-.fence_bound {
-  background-color: darkgray;
-  border: 2px solid lightgray;
-}
-
-.hole {
-  background-color: black;
-}
-
-.grass {
-  background-color: yellowgreen;
-  border: 2px dotted dimgray;
-}
-
-.deepsea,
-.xbdpsea {
-  /* 清澈水域，为啥有两个得问YJ */
-  background-color: steelblue;
-  border: 2px dotted darkgray;
-}
-
-.infection,
-.corrosion_2,
-.healing,
-.telin,
-.telout,
-.volcano,
-.volspread {
-  background-color: darkgray;
-  border: 2px dotted dimgray;
-  background-position: center;
-  background-size: contain;
-  background-repeat: no-repeat;
-}
-
-.corrosion_2 span {
-  color: darkgoldenrod;
-}
-
-.telin,
-.telout {
-  border: 2px dashed dimgray;
-}
-
-.telin {
-  transform: rotate(180deg); /* fa 和 windicss 都不起作用 */
-}
-
-.token {
-  background-position: center;
-  background-size: contain;
-  background-repeat: no-repeat;
-}
-
-.hiddenstone {
-  /* 遗迹残骸 */
-  background-color: dimgray;
-  border: 2px solid darkgray;
-}
-
-.streasure span {
-  /* 宝刺金属箱 */
-  color: black;
-}
-
-.roadblock {
-  /* 道路障碍物 */
-  background-color: slategray;
-  border: 2px dotted darkgray;
-}
-
-.hstone span {
-  /* 城防路障 */
-  color: #745d32;
-}
-
-.xbmgbird span {
-  /* 喙中奇物 */
-  color: #653409;
-}
-
-.airsup {
-  /* 空袭侵入点 */
-  background-color: indianred;
-  border: 2px solid darkred;
-}
-
-.wdescp span, /* 逃脱点 */
-.xbbase span, /* 基地 */
-.xbfdtion span, /* 风沙营垒 */
-.xbmcv span /* 矿业房车 */ {
-  color: green;
-}
-
-.tower span {
-  /* 留声机 */
-  color: darkslategray;
-}
-
-.redtower span /* 移动战塔 */
-.xbstation span /* 号令点 */ {
-  color: crimson;
-}
-
-/* 池沼地块 */
-.puddle {
-  background-color: #599bd1;
-  border: 2px dotted darkgray;
-}
-.puddle span {
-  color: #82bded;
-}
-
-/* 隐焰地块 */
-.steam {
-  background-color: #935858;
-  border: 2px dotted darkgray;
-}
-.steam span {
-  color: #973a3a;
-}
-
-.xbrandprop span {
-  color: rgb(255, 229, 194);
-}
-
-.xbcp span {
-  color: rgb(255, 255, 99);
 }
 </style>
---
+++ src/widgets/XbMapViewer/Map.vue
@@ -0,0 +1,139 @@
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+
+import Block from "./Block.vue";
+import { type XbConstData } from "./consts";
+
+export interface Props {
+  map: {
+    options: Record<string, any>;
+    mapData: Record<string, any>;
+    predefines: Record<string, any>;
+  };
+  embed?: boolean;
+  xbMapConst: XbConstData;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  embed: false,
+});
+
+const tokens = computed(() => {
+  const result: Record<string, any> = {};
+  const tokenInsts = props.map?.predefines.tokenInsts || [];
+  for (const token of tokenInsts) {
+    const pos = token.position;
+    const coord = `${pos.row}-${pos.col}`;
+    result[coord] = result[coord] ?? [];
+    result[coord].push(token.inst.characterKey.replaceAll(/trap_\d+_/g, ""));
+  }
+  return result;
+});
+
+const black = computed(() => {
+  const result: Record<string, string> = {};
+  const configBlackBoard = props.map?.options.configBlackBoard || [];
+  for (const block of configBlackBoard) {
+    const val = block.valueStr;
+    if (val === null) continue;
+
+    const pos = val.replaceAll(/\(|\)/g, "").split(",");
+    if (pos.length === 4) {
+      // let list = []
+      for (let y = Number.parseInt(pos[0]); y <= Number.parseInt(pos[2]); y++) {
+        for (
+          let x = Number.parseInt(pos[1]);
+          x <= Number.parseInt(pos[3]);
+          x++
+        ) {
+          result[`${y}-${x}`] = "rect";
+        }
+      }
+    }
+  }
+
+  return result;
+});
+
+function getTile(index: number) {
+  let a = props.map?.mapData.tiles[index].tileKey.replace("tile_", "");
+  if (a === "floor" || a === "road") {
+    const buildableType = props.map?.mapData.tiles[index].buildableType;
+    const isRoad =
+      buildableType === 1 ||
+      buildableType === "MELEE" ||
+      buildableType === "ALL";
+    a = isRoad ? "road" : "floor";
+  }
+  return a;
+}
+
+function getToken(row: number, col: number) {
+  return tokens.value[`${row}-${col}`];
+}
+
+const rootRef = ref<HTMLElement>();
+const fontsize = ref("");
+const bordersize = ref("");
+const width = computed(() => {
+  const mapData = props.map.mapData;
+  return mapData.width ?? mapData.map[0].length;
+});
+
+onMounted(() => {
+  if (!rootRef.value || !props.map) return;
+
+  fontsize.value = `${
+    (rootRef.value.getBoundingClientRect().width /
+      width.value /
+      (props.embed ? 5 : 9)) *
+    5
+  }px`;
+
+  bordersize.value = width.value <= 25 ? "2px" : "1px";
+});
+</script>
+
+<template>
+  <div :id="embed ? 'mapmodal' : 'map'" ref="rootRef" class="xbmap w-full">
+    <div v-for="(row, i) in map.mapData.map" :key="i" class="row">
+      <Block
+        v-for="(board, n) in row"
+        :key="i * row.length + n"
+        :tile="getTile(board)"
+        :tile-height-type="map.mapData.tiles[board].heightType.toString()"
+        :tokens="getToken(map.mapData.map.length - 1 - i, n)"
+        :black="black && black[`${map.mapData.map.length - 1 - i}-${n}`]"
+        :blockmap="xbMapConst.blockmap"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#map .row {
+  display: grid;
+  grid-template-columns: repeat(v-bind(width), 1fr);
+}
+
+#map :deep(.block span) {
+  font-size: v-bind(fontsize);
+}
+
+#map :deep(.block) {
+  border-width: v-bind(bordersize);
+}
+
+#mapmodal .row {
+  display: grid;
+  grid-template-columns: repeat(v-bind(width), 1fr);
+}
+
+#mapmodal :deep(.block span) {
+  font-size: v-bind(fontsize);
+}
+
+#mapmodal :deep(.block) {
+  border-width: v-bind(bordersize);
+}
+</style>
