e254a0d64eb8fc42cd4bf65ddfe48a559884bc0b
StarHeartHunt <starheart233@gmail.com>
2025-04-11 13:02:24 +0800

    fix: re-enable pre-multiplied alpha

--- src/entries/SpineViewer.ts
+++ src/entries/SpineViewer.ts
@@ -21,21 +21,6 @@ function main() {
       "https://static.prts.wiki/spine38/",
     );
   }
-  // 一个临时的patch 这个变更在spine runtime 4.1上 要是以后升到4.1了就可以删掉了
-  // 现在的spine runtime渲染的时候 给blendFunc传的都是gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA
-  // https://github.com/EsotericSoftware/spine-runtimes/commit/78a730a6d78241add86bef41aa26530567bc11dc#diff-b88f26766af82544f5c7781e19787743f71b053a06d24e7ee0bc6fe7975f2f72L63-L73
-  // 有的spine不需要这个patch (比如年的)
-  if (!ele?.dataset.disablePatch) {
-    WebGL2RenderingContext.prototype.blendFunc = function () {
-      WebGL2RenderingContext.prototype.blendFuncSeparate.call(
-        this,
-        WebGL2RenderingContext.SRC_ALPHA,
-        WebGL2RenderingContext.ONE_MINUS_SRC_ALPHA,
-        WebGL2RenderingContext.ONE,
-        WebGL2RenderingContext.ONE_MINUS_SRC_ALPHA,
-      );
-    };
-  }
 
   if (ele)
     createApp(SpineVue, { conf: spineData, id: ele?.dataset.id }).mount(ele);
