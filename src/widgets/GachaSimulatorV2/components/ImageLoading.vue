7e373deae6da53847c8ab77a74a9b6e6bf0d57a4
StarHeartHunt <starheart233@gmail.com>
2025-05-14 18:53:40 +0800

    perf: remove redundant code

--- src/widgets/GachaSimulatorV2/components/ImageLoading.vue
+++ src/widgets/GachaSimulatorV2/components/ImageLoading.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
 
-import { NAlert, NSpin } from "naive-ui";
+import { NSpin } from "naive-ui";
 
 const props = defineProps<{
   class?: any;
@@ -11,59 +11,30 @@ const props = defineProps<{
 }>();
 
 const isLoading = ref(true);
-const hasError = ref(false);
 
 const handleLoad = () => {
   isLoading.value = false;
-  hasError.value = false;
-};
-
-const handleError = () => {
-  isLoading.value = false;
-  hasError.value = true;
 };
 
 watch(
   () => props.src,
   () => {
     isLoading.value = true;
-    hasError.value = false;
   },
   { immediate: true },
 );
 </script>
 
 <template>
-  <div class="image-loading">
-    <NSpin :show="isLoading" size="large">
-      <img
-        :class="props.class"
-        :src="props.src"
-        :alt="props.alt"
-        :fallback="props.fallback"
-        @load="handleLoad"
-        @error="handleError"
-      />
-    </NSpin>
-    <n-alert
-      v-if="hasError"
-      type="error"
-      title="Error"
-      description="Failed to load image."
-      class="error-alert"
+  <NSpin :show="isLoading" size="large">
+    <img
+      :class="props.class"
+      :src="props.src"
+      :alt="props.alt"
+      :fallback="props.fallback"
+      @load="handleLoad"
     />
-  </div>
+  </NSpin>
 </template>
 
-<style scoped>
-.image-loading {
-  position: relative;
-  display: inline-block;
-  height: 100%;
-  text-align: center;
-}
-
-.error-alert {
-  margin-top: 10px;
-}
-</style>
+<style scoped></style>
