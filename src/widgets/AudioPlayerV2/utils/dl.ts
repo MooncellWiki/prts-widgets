84fdfe701a42978ca1db23e98b122391faae43a6
qwq233 <qwq233@qwq2333.top>
2025-07-14 15:47:18 +0800

    feat: download audio in zip format
    This commit stopped automatically combining audio for users.
    
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- src/widgets/AudioPlayerV2/utils/dl.ts
+++ src/widgets/AudioPlayerV2/utils/dl.ts
@@ -1,27 +1,32 @@
-import Crunker from "crunker";
+import saveAs from "file-saver";
+import JSZip from "jszip";
 
-export function combineAndDownload(url1: string, url2: string) {
-  const crunker = new Crunker();
+export async function combineAndDownload(url1: string, url2: string) {
+  try {
+    const zip = new JSZip();
 
-  crunker
-    .fetchAudio(url1, url2)
-    .then((buffers) => {
-      return crunker.concatAudio(buffers);
-    })
-    .then((merged) => {
-      return crunker.export(merged, `audio/${url1.split(".").pop()}`);
-    })
-    .then((output) => {
-      crunker.download(
-        output.blob,
-        url1.split("/").pop()?.split(".")[0].replace("_intro", "") ||
-          "combined_audio.mp3",
-      );
-      console.log(output.url);
-    })
-    .catch((error) => {
-      console.error("Audio combination failed:", error);
-    });
+    const getFilename = (url: string): string =>
+      url.slice(Math.max(0, url.lastIndexOf("/") + 1)) || "file1";
+    const zipFilename = getFilename(url1).replace("_intro", "");
+
+    const addFileToZip = async (url: string, filename?: string) => {
+      const response = await fetch(url);
+      if (!response.ok)
+        throw new Error(`Download failed: ${response.statusText}`);
+      const blob = await response.blob();
+      zip.file(filename || getFilename(url), blob);
+    };
+
+    await Promise.all([
+      addFileToZip(url1, getFilename(url1)),
+      addFileToZip(url2, getFilename(url2)),
+    ]);
+
+    const content = await zip.generateAsync({ type: "blob" });
+    saveAs(content, `${zipFilename}.zip`);
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
 }
 
 export function downloadFile(name: string, src: string) {
