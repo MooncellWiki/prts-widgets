47141947774f000686471a0dc9d36a6d5800ce55
qwq233 <qwq233@qwq2333.top>
2025-07-16 23:50:35 +0800

    fix: unable to download
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- src/widgets/AudioPlayerV2/utils/dl.ts
+++ src/widgets/AudioPlayerV2/utils/dl.ts
@@ -1,6 +1,9 @@
 export function downloadFile(name: string, src: string) {
   const ele = document.createElement("a");
-  ele.setAttribute("href", src);
+  ele.setAttribute("href", `${src}?filename=${encodeURIComponent(name)}`);
   ele.setAttribute("download", name);
+  ele.setAttribute("target", "_blank");
+  document.body.append(ele);
   ele.click();
+  ele.remove();
 }
